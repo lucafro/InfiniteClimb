04f05647a83bae0b0f7f5f1dbe6a4850cca2a4e7
lucafro <lucafrosler@gmail.com>
2023-11-12 16:13:50 +0100

    Set up rocks with dissolve shader

---
+++ Assets/CustomAssets/Scripts/PlatformManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlatformManager : MonoBehaviour
+{
+    public GameObject XrRig;
+    public GameObject NonVrPlayer;
+
+    [SerializeField]
+    private bool _VR = true;
+
+    private void Awake()
+    {
+        if (XrRig != null && NonVrPlayer != null)
+        {
+            if (_VR)
+            {
+                XrRig.SetActive(true);
+                NonVrPlayer.SetActive(false);
+            }
+            else
+            {
+                XrRig.SetActive(false);
+                NonVrPlayer.SetActive(true);
+            }
+        }
+    }
+}
